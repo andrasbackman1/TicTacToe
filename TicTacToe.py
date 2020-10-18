f18518e7139e6368744d954db0f3d4146ca3c255
andrasbackman1 <63351159+andrasbackman1@users.noreply.github.com>
2020-10-18 17:29:37 +0100

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,30 +2,6 @@ import tkinter as tk
 import sys
 import numpy as np
 
-"""
-Vey basic setup for a Tic Tac Toe game.
-
-It is implemented using tkinter.
-
-The actual buttons you push and the board itself are two separate classes.
-I decided to separate them because I could not figure out a better
-way to store the color and position index of the "plates".
-
-I used numpy to check the state of the board at all times. I 
-called it a virtual board. I figure that it would be a lot faster
-to use numpy's indexing to check if someone had won than to use
-the built in stuctures. Atleast it was alot faster to write
-because Numpy is something I am actually somewhat comfortable
-working with.
-
-I got the problem that suddenly "self" in the functions for the TicTacToe
-became a Plate instance. I do not know why. I also could not get attributes
-from the TicTacToe class. The only solution I found was to declare the 
-variable as global -- not a good fix but it works so...
-
-It is very good to practice using classes and objects in this way.
-Learned a lot on the way. Will have to figure somthing else out next.
-"""
 class Plate:
     def __init__ (self, frame, row, column, color = "gray"):
         self.row = row
